9e254c85a230e2a34c9e4361a7425943a03a6388
amoren86 <amoren86@xtec.cat>
2026-09-21 23:34:30 +0200

    Update HelloWorldServlet to correct servlet name in output

--- hello-world-servlet/src/main/java/cat/institutmarianao/hello/world/servlet/HelloWorldServlet.java
+++ hello-world-servlet/src/main/java/cat/institutmarianao/hello/world/servlet/HelloWorldServlet.java
@@ -31,7 +31,7 @@ public class HelloWorldServlet extends HttpServlet {
 			out.println("</head>");
 			out.println("<body>");
 			out.println("	<header>");
-			out.println("		<h1>Servlet HelloWorldXml at " + request.getContextPath() + "</h1>");
+			out.println("		<h1>Servlet HelloWorld at " + request.getContextPath() + "</h1>");
 			out.println("		<p>An app by Institut Marianao</p>");
 			out.println("	</header>");
 			out.println("</body>");
