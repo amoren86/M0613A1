176589b133a0f08872cb4206c1ada36b6beafa00
amoren86 <amoren86@xtec.cat>
2026-09-23 22:07:45 +0200

    Refactor GuessColor2Servlet: update servlet initialization, improve HTML structure, and remove obsolete files

--- guess-color2-servlet/src/main/java/cat/institutmarianao/guess/color/servlet/GuessColor2Servlet.java
+++ guess-color2-servlet/src/main/java/cat/institutmarianao/guess/color/servlet/GuessColor2Servlet.java
@@ -2,37 +2,40 @@ package cat.institutmarianao.guess.color.servlet;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/guess-color", initParams = { @WebInitParam(name = "color", value = "red") })
 public class GuessColor2Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private String initColor;
 
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
 	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-		initColor = config.getInitParameter("color");
+	public void init() throws ServletException {
+		initColor = getServletConfig().getInitParameter("color");
 	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
-
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		// L'usuari ha seleccionat un color i ho ha enviat.
 		String paramColor = request.getParameter("color");
 
 		if (paramColor == null) {
-			response.sendRedirect("guess_color_2.html");
+			response.sendRedirect("guess_color.html");
 		} else if (initColor.equalsIgnoreCase(paramColor)) {
 			response.sendRedirect("win.html");
 		} else {
